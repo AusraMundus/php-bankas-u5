234ab7cc5b55a1e26ea6d8b378d5fa0f69a1f915
Ausra <munduswebstudio@gmail.com>
2023-07-14 15:49:35 +0300

    Accounts table connected with Clients table

--- bankasu5/resources/views/clients/delete.blade.php
+++ bankasu5/resources/views/clients/delete.blade.php
@@ -5,25 +5,24 @@
     <div class="row justify-content-center">
         <div class="col-md-4">
             <div class="card">
+            <h5 class="card-header">Confirm client delete</h5>
                 <div class="card-body">
-                    <h5 class="card-title">Confirm client delete</h5>
-                    <form method="post" action="{{route('clients-destroy', $client)}}">
-                        <div class="justify-content-between">
-                            <div>
-                                <div class="d-flex">
+                    <h6 class="card-subtitle mb-2 text-muted">Are you sure you want to delete this client?</h6>
+                        <form method="post" action="{{route('clients-destroy', $client)}}">
+                            <div class="justify-content-between">
+                                <div class="d-flex mb-3">
                                     <div class="ms-2">
-                                        <div>{{$client->first_name}}</div>
-                                        <div>{{$client->last_name}}</div>
-                                        <div>{{$client->personal_id}}</div>
+                                        <div class="fw-bold fs-3">{{$client->first_name}} {{$client->last_name}}</div>
+                                        <div>Personal ID</div>
+                                        <div class="fw-bold">{{$client->personal_id}}</div>
                                     </div>
                                 </div>
                             </div>
-                        </div>
-                        <button type="submit" class="btn btn-danger">Delete</button>
-                        <a href="{{route('clients-index')}}" class="btn btn-secondary">Cancel</a>
-                        @method('delete')
-                        @csrf
-                    </form>
+                            <button type="submit" class="btn btn-danger m-1">Delete</button>
+                            <a class="btn btn-secondary m-1" href="{{route('clients-index')}}">Cancel</a>
+                            @method('delete')
+                            @csrf
+                        </form>
                 </div>
             </div>
         </div>
